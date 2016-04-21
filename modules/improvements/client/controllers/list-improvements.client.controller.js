28a8e2c7c92066d466014a8696e020a7c64eb02e
EmmaPuls <got2love@gmail.com>
2016-04-21 15:26:25 +0800

    Improvement load list per submission on new page - Included improvement list searching

--- modules/improvements/client/controllers/list-improvements.client.controller.js
+++ modules/improvements/client/controllers/list-improvements.client.controller.js
@@ -11,5 +11,33 @@
     var vm = this;
 
     vm.improvements = ImprovementsService.query();
+
+    vm.sortTerm = 'date'; // Sort by date by default
+    vm.sortBtnText = 'Sort by Date'; // The value to display in the sort button
+    vm.sortReverse = false; // Sort in ascending order by default
+    vm.sortMenuOpen = false; // The sort menu starts off closed by default
+
+    // The code for managing the sorting functionality
+    vm.sortBy = function(inSortTerm) {
+
+      // Change the value of the search term to the imported value
+      switch (inSortTerm) {
+        case 'user':
+          vm.sortTerm = 'user.displayName';
+          vm.sortBtnText = 'Sort by User';
+          break;
+        default:
+          vm.sortTerm = 'date';
+          vm.sortBtnText = 'Sort by Date';
+          break;
+      }
+
+      // Change the sorting order on every click
+      vm.sortReverse = !vm.sortReverse;
+    };
+
+    vm.toggleSortMenu = function() {
+      vm.sortMenuOpen = !vm.sortMenuOpen;
+    };
   }
 })();

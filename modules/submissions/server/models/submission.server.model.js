b19c0c2605372045db1f7f025b02fecea844b7b6
ycAlex <yc000666li@gmail.com>
2016-04-25 17:56:48 +0800

    testing

--- modules/submissions/server/models/submission.server.model.js
+++ modules/submissions/server/models/submission.server.model.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose'),
 
 /**
  * Submission Schema
- * Each submission must be tied to a user dsad
+ * Each submission must be tied to a user sdsadsad
  */
 var SubmissionSchema = new Schema({
   title: {

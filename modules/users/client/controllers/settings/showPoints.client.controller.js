12063bbb4cd6a953fb29086b3940bb74c5f5b367
Yibbies <yeo.josh@y7mail.com>
2016-05-01 19:52:31 +0800

    Added progress bar to settings page.
    Progress bar should update dynamically as points are allocated to the
    user.

--- modules/users/client/controllers/settings/showPoints.client.controller.js
+++ modules/users/client/controllers/settings/showPoints.client.controller.js
@@ -27,13 +27,15 @@ angular.module('users').controller('showPointsController', ['$scope', '$http', '
     function ($scope, $http, $location, Users, Authentication) {
         $scope.user = Authentication.user;
 
-
         // Update a user profile
         $scope.getPoints = function () {
             Users.query(function(users){
                 $scope.users = users;
             });
-
         };
+        // Piggyback off query to update points
+        $scope.progress = (user.points/100)*100;
+
+            //TODO: create function that grows progress bar based on points
     }
 ]);
